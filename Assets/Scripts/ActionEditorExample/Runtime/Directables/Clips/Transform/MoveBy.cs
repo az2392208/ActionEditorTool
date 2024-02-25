1a9562ef5e9e99a5fa40bccc452dcf3b8fbbcbad
Aive <az2392208@gmail.com>
2024-02-25 23:05:40 +0800

    第一次提交文件

---
+++ Assets/Scripts/ActionEditorExample/Runtime/Directables/Clips/Transform/MoveBy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NBC.ActionEditor;
+using UnityEngine;
+
+namespace NBC.ActionEditorExample
+{
+    [Name("移动")]
+    [Description("将对象移动一定距离")]
+    [Color(70f / 255f, 1, 140f / 255f)]
+    [Attachable(typeof(ActionTrack))]
+    public class MoveBy : ActionClip
+    {
+        [SerializeField] [HideInInspector] private float length = 1;
+
+        [MenuName("运动曲线")] public AnimationCurve curve;
+
+        [MenuName("运动补间")] public EaseType interpolation = EaseType.QuadInOut;
+
+        [MenuName("移动量")] public Vector3 move;
+
+        public override string info => $"位移:\n{move}";
+
+        public override float Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+
+        public override bool isValid => true;
+
+        private ActionTrack Track => (ActionTrack)Parent;
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# 使用说明
+点击菜单`NBC/Action Editor/Open Action Editor`打开编辑窗口既可
+
+
+# 文档
+操作手册文档：[点击查看](https://nobug.cn/book/414447506088261)
+
+# 预览视频
+
+https://www.bilibili.com/video/BV1tM4y1q7xg/
+
+# Runtime演示说明
+
+runtime下示例为用法演示，使用的是异步队列，大家可以参考然后根据自己喜好编写对应逻辑。
+
+有疑惑查看Runtime演示目录下的README.md文件。
+
+
+# 交流Q群
+
+567604178
+
+# 其他演示项目
+
+- 2D回合制项目演示：等待补充
+- 3D回合制项目演示：等待补充
+- ARPG项目验收：等待补充
